256fdcccad288b38d2f208448f1bd22689d932e3
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2022-04-21 16:23:59 +0200

    Split impls and added lendl in profile area

--- pyroll_lendl_equivalent_method/hookimpls.py
+++ pyroll_lendl_equivalent_method/hookimpls.py
@@ -1,33 +1,68 @@
 import numpy as np
 
-from pyroll import Profile, RollPass
+from pyroll import Profile, RollPass, RollPassOutProfile, RollPassInProfile
 from collections import namedtuple
 
 from shapely import ops
 
-from shapely.geometry import LineString
+from shapely.geometry import LineString, Polygon
 
 
 @RollPass.hookimpl
-def lendl_width(roll_pass: RollPass):
+def in_profile_groove_intersection_points(roll_pass: RollPass):
     upper_intersection_points = roll_pass.in_profile.upper_contour_line.intersection(roll_pass.groove.contour_line)
     lower_intersection_points = roll_pass.in_profile.lower_contour_line.intersection(roll_pass.groove.contour_line)
 
     intersection_points = ops.unary_union([upper_intersection_points, lower_intersection_points])
 
-    left_lendl_width_points = []
-    right_lendl_width_points = []
+    return intersection_points
 
-    for point in intersection_points.geoms:
+
+@RollPass.hookimpl
+def left_lendl_width_boundary(roll_pass: RollPass):
+    left_lendl_width_points = []
+
+    for point in roll_pass.in_profile_groove_intersection_points.geoms:
         if point.x < 0:
             left_lendl_width_points.append(point)
-        else:
-            right_lendl_width_points.append(point)
 
     left_boundary = LineString(left_lendl_width_points)
+
+    return left_boundary
+
+
+@RollPass.hookimpl
+def right_lendl_width_boundary(roll_pass: RollPass):
+    right_lendl_width_points = []
+
+    for point in roll_pass.in_profile_groove_intersection_points.geoms:
+        if point.x > 0:
+            right_lendl_width_points.append(point)
+
     right_boundary = LineString(right_lendl_width_points)
 
-    return left_boundary.distance(right_boundary)
+    return right_boundary
+
+
+@RollPass.hookimpl
+def lendl_width(roll_pass: RollPass):
+    return roll_pass.left_lendl_width_boundary.distance(roll_pass.right_lendl_width_boundary)
+
+
+@RollPassInProfile.hookimpl
+def lendl_in_profile_area(roll_pass: RollPass):
+    area_as_multi_line = ops.unary_union([roll_pass.left_lendl_width_boundary,
+                                          roll_pass.right_lendl_width_boundary,
+                                          roll_pass.groove.contour_line])
+
+    merged_line = ops.linemerge(area_as_multi_line)
+    area_as_polygon = Polygon(merged_line)
+    return area_as_polygon.area
+
+
+@RollPassOutProfile.hookimpl
+def lendl_out_profile_area():
+    pass
 
 
 @Profile.hookimpl
