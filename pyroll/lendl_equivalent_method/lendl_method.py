b9ac012a74582cbf47c519b8e2b318fd3dd92efe
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-01-27 15:54:02 +0100

    impl for 3-roll

--- pyroll/lendl_equivalent_method/lendl_method.py
+++ pyroll/lendl_equivalent_method/lendl_method.py
@@ -1,7 +1,8 @@
 import math
 
 from pyroll.core import RollPass, Hook, ThreeRollPass
-from shapely import intersection, MultiLineString, Polygon
+from shapely import intersection, MultiLineString, Polygon, unary_union
+from shapely.affinity import rotate
 from shapely.ops import clip_by_rect
 import numpy as np
 
@@ -28,6 +29,40 @@ def lendl_width(self: RollPass):
 def in_lendl_section(self: RollPass.InProfile):
     half_width = self.roll_pass.lendl_width / 2
     return clip_by_rect(self.cross_section, -half_width, -math.inf, half_width, math.inf)
+
+
+@RollPass.lendl_width
+def lendl_width_3fold(self: RollPass):
+    i = intersection(self.roll_pass.in_profile.cross_section, self.contour_lines[1])
+    return i.width
+
+
+def clip_3fold(poly: Polygon, roll_pass: ThreeRollPass):
+    half_width = roll_pass.lendl_width / 2
+    poly = clip_by_rect(poly, -half_width, -math.inf, half_width, 0)
+
+    return unary_union(
+        [
+            rotate(poly, angle=a, origin=(0, 0))
+            for a in (0, 120, 240)
+        ]
+    )
+
+
+@ThreeRollPass.InProfile.lendl_section
+def in_lendl_section_3fold(self: ThreeRollPass.InProfile):
+    poly = Polygon(
+        np.concatenate([ls.coords for ls in self.cross_section])
+    )
+    return clip_3fold(poly, self.roll_pass)
+
+
+@ThreeRollPass.OutProfile.lendl_section
+def out_lendl_section_3fold(self: ThreeRollPass.OutProfile):
+    poly = Polygon(
+        np.concatenate([ls.coords for ls in self.roll_pass.contour_lines])
+    )
+    return clip_3fold(poly, self.roll_pass)
 
 
 @RollPass.Profile.equivalent_width
